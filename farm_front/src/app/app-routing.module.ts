665bb074bc94bfee2e9f29cc8efb439a360b8e62
Victor Batista <48859696+Lonyless@users.noreply.github.com>
2023-01-29 23:02:46 -0300

    adding toolbar and routing

--- farm_front/src/app/app-routing.module.ts
+++ farm_front/src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component'
 import { FarmComponent } from './farm/farm.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
+import { FarmCadastroComponent } from './farm-cadastro/farm-cadastro.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'farm', component: FarmComponent },
+  { path: '', component: FarmComponent },
+  { path: 'cadastro', component: FarmCadastroComponent },
+  { path: 'cadastro/:id', component: FarmCadastroComponent },
+
 ];
 
 @NgModule({
