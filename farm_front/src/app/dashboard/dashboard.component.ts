ea7017c1538105749e61354bd29683a38da9e4b9
Victor Batista <48859696+Lonyless@users.noreply.github.com>
2023-01-29 23:04:33 -0300

    creating dashboard component

--- farm_front/src/app/dashboard/dashboard.component.ts
+++ farm_front/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
+import { FarmService } from '../farm/services/farm.service';
+import { Router } from '@angular/router';
+import { Farm } from '../farm/models/Farm';
 
 @Component({
   selector: 'app-dashboard',
-  template: `<p routerLink="farm">Cadastrar</p>`,
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+
+  constructor(private _farmService: FarmService, private router: Router) { }
+
+  ngOnInit() {
+    this.fetch()
+  }
+
+  dataSource: Farm[] = [];
+
+  fetch(search?: string) {
+    this._farmService.list(search).subscribe((data) => {
+      this.dataSource = data
+    })
+  }
+
+}
